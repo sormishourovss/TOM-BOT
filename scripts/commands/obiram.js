c940b5ef9a582b99ff8ea18cddce1f9c64d12d0a
RAKIB CHOWDHURY <152077471+SYSTEM-ERROR-KING@users.noreply.github.com>
2024-02-27 18:20:51 +0600

    Create obiram.js

---
+++ scripts/commands/obiram.js
@@ -0,0 +1,27 @@
+  const fs = require("fs");
+module.exports.config = {
+      name: "🌺",
+    version: "1.1.0",
+    permission: 0,
+    credits: "farhan",
+    description: "noprefix",
+    prefix: true,
+    category: "commands",
+    usages: "🌺",
+    cooldowns: 5,
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+  var { threadID, messageID } = event;
+  if (event.body.indexOf("🌺")==0 || event.body.indexOf("🌸")==0 || event.body.indexOf("🥀")==0 || event.body.indexOf("🌹")==0) {
+    var msg = {
+        body: "~  ভালোবাসা অবিরাম কলিজা..!!🥰🥀",
+        attachment: fs.createReadStream(__dirname + `/noprefix/valobashaoviram.mp3`)
+      }
+      api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("🥺", event.messageID, (err) => {}, true)
+    }
+  }
+  module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
