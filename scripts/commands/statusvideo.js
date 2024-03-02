44723ede23c8c41cbd2a1c674045095db8f899f2
SHOUROV SS <161962306+sormishourov@users.noreply.github.com>
2024-03-03 00:20:14 +0600

    Update statusvideo.js

--- scripts/commands/statusvideo.js
+++ scripts/commands/statusvideo.js
@@ -20,7 +20,7 @@ module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currenc
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
-   var hi = ["--𝐒𝐭𝐚𝐭𝐮𝐬 𝐯𝐢𝐝𝐞𝐨💕🌸 --𝐅𝐚𝐫𝐡𝐚𝐧-𝐈𝐬𝐥𝐚𝐦--"];
+   var hi = ["--𝐒𝐭𝐚𝐭𝐮𝐬 𝐯𝐢𝐝𝐞𝐨💕🌸 --𝐒𝐇𝐎𝐔𝐑𝐎𝐕-𝐁𝐎𝐓--"];
   var know = hi[Math.floor(Math.random() * hi.length)];
   var link = [
 
