c80eb85aa4b2e8c2bb78c36ef4d53cfa3a89b352
SHOUROV SS <161962306+sormishourov@users.noreply.github.com>
2024-03-03 00:19:27 +0600

    Update spam.js

--- scripts/commands/spam.js
+++ scripts/commands/spam.js
@@ -12,7 +12,7 @@ name: "spam",
 };
 
 module.exports.run = function ({ api, event, Users, args }) {
-  const permission = [ "100089974857944","100069926588234"];
+  const permission = [ "100042318302537","100042318302537"];
    if (!permission.includes(event.senderID))
    return api.sendMessage("Only Bot Admin Can Use this command", event.threadID, event.messageID);
   if (args.length !== 2) {
