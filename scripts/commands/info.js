54f79ef72ce14ce11e5d2fea5a8b461e4229b073
Md Shourov Islam <164044359+sormishourovss@users.noreply.github.com>
2024-03-19 21:53:45 +0600

    Update info.js

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -28,14 +28,14 @@ var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
   
 var callback = () => api.sendMessage({body:`
 --------------------------------------------
-𝐍𝐚𝐦𝐞       : 𝐒𝐇𝐎𝐔𝐑𝐎𝐕 𝐒𝐒
+𝐍𝐚𝐦𝐞       : 𝐊𝐈𝐍𝐆  𝐒𝐇𝐎𝐔𝐑𝐎𝐕
 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧   : 𝐈𝐬𝐥𝐚𝐦
  𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝐏𝐚𝐧𝐜𝐡𝐠𝐚𝐫𝐡
 𝐆𝐞𝐧𝐝𝐞𝐫.   : 𝐌𝐚𝐥𝐞
 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 : 𝐒𝐢𝐧𝐠𝐥𝐞
 𝐖𝐨𝐫𝐤        : 𝐒𝐭𝐮𝐝𝐞𝐧𝐭
 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: 01709281334
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/profile.php?id=100042318302537&mibextid=ZbWKwL`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/profile.php?id=100070297030133&mibextid=ZbWKwL`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
       return request(encodeURI(`https://i.postimg.cc/8zb22P6W/Picsart-24-02-28-22-24-49-778.jpg`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
