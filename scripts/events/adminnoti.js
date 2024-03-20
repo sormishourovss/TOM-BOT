1e16355bc21129474e12c9ff1dd26d3a849095ba
Md Shourov Islam <164044359+sormishourovss@users.noreply.github.com>
2024-03-20 16:46:08 +0600

    Update adminnoti.js

--- scripts/events/adminnoti.js
+++ scripts/events/adminnoti.js
@@ -37,10 +37,10 @@ module.exports.run = async function({ event, api, Threads, Users }) {
       case "log:thread-admins": {
         if (logMessageData.ADMIN_EVENT === "add_admin") {
           dataThread.adminIDs.push({ id: logMessageData.TARGET_ID });
-          api.sendMessage(`[ 𝐒𝐇𝐎𝐔𝐑𝐎𝐕-𝐁𝐎𝐓 ]\n❯ USER UPDATE ${Users.getNameUser(logMessageData.TARGET_ID)} Became a group admin`, threadID);
+          api.sendMessage(`[ 𝐀𝐃.𝐂𝐄𝐎 𝐒𝐇𝐎𝐔𝐑𝐎𝐕 ]\n❯ USER UPDATE ${Users.getNameUser(logMessageData.TARGET_ID)} Became a group admin`, threadID);
         } else if (logMessageData.ADMIN_EVENT === "remove_admin") {
           dataThread.adminIDs = dataThread.adminIDs.filter(item => item.id !== logMessageData.TARGET_ID);
-          api.sendMessage(`[ 𝐒𝐇𝐎𝐔𝐑𝐎𝐕-𝐁𝐎𝐓 ]\n❯ Remove user's admin position ${logMessageData.TARGET_ID}`, threadID);
+          api.sendMessage(`[ 𝐀𝐃.𝐂𝐄𝐎 𝐒𝐇𝐎𝐔𝐑𝐎𝐕 ]\n❯ Remove user's admin position ${logMessageData.TARGET_ID}`, threadID);
         }
         break;
       }
@@ -51,20 +51,20 @@ module.exports.run = async function({ event, api, Threads, Users }) {
           dataThread.nicknames[participant_id] = nickname;
           const participantName = await Users.getNameUser(participant_id);
           const formattedNickname = nickname || "deleted nickname";
-          api.sendMessage(`[ 𝐒𝐇𝐎𝐔𝐑𝐎𝐕-𝐁𝐎𝐓 ]\n❯ Updated nickname for ${participantName}: ${formattedNickname}.`, threadID);
+          api.sendMessage(`[ 𝐀𝐃.𝐂𝐄𝐎 𝐒𝐇𝐎𝐔𝐑𝐎𝐕 ]\n❯ Updated nickname for ${participantName}: ${formattedNickname}.`, threadID);
         }
         break;
       }
       case "log:thread-name": {
         dataThread.threadName = logMessageData.name || null;
-        api.sendMessage(`[ 𝐒𝐇𝐎𝐔𝐑𝐎𝐕-𝐁𝐎𝐓 ]\n❯ ${(dataThread.threadName) ? `Updated Group Name to: ${dataThread.threadName}` : 'Cleared the Group Name'}.`, threadID);
+        api.sendMessage(`[ 𝐀𝐃.𝐂𝐄𝐎 𝐒𝐇𝐎𝐔𝐑𝐎𝐕 ]\n❯ ${(dataThread.threadName) ? `Updated Group Name to: ${dataThread.threadName}` : 'Cleared the Group Name'}.`, threadID);
         break;
       }
       case "log:thread-icon": {
         const preIcon = JSON.parse(fs.readFileSync(iconPath));
         dataThread.threadIcon = logMessageData.thread_icon || "👍";
         if (global.configModule[this.config.name].sendNoti) {
-          api.sendMessage(`[ 𝐒𝐇𝐎𝐔𝐑𝐎𝐕-𝐁𝐎𝐓 ]\n❯ ${logMessageBody.replace("emoji", "icon")}\n❯ Original Emoji: ${preIcon[threadID] || "unknown"}`, threadID, async (error, info) => {
+          api.sendMessage(`[ 𝐀𝐃.𝐂𝐄𝐎 𝐒𝐇𝐎𝐔𝐑𝐎𝐕 ]\n❯ ${logMessageBody.replace("emoji", "icon")}\n❯ Original Emoji: ${preIcon[threadID] || "unknown"}`, threadID, async (error, info) => {
             preIcon[threadID] = dataThread.threadIcon;
             fs.writeFileSync(iconPath, JSON.stringify(preIcon));
             if (global.configModule[this.config.name].autoUnsend) {
@@ -78,17 +78,17 @@ module.exports.run = async function({ event, api, Threads, Users }) {
       case "log:thread-call": {
         if (logMessageData.event === "group_call_started") {
           const name = await Users.getNameUser(logMessageData.caller_id);
-          api.sendMessage(`[ 𝐒𝐇𝐎𝐔𝐑𝐎𝐕-𝐁𝐎𝐓 ]\n❯ ${name} STARTED A ${(logMessageData.video) ? 'VIDEO ' : ''}CALL.`, threadID);
+          api.sendMessage(`[ 𝐀𝐃.𝐂𝐄𝐎 𝐒𝐇𝐎𝐔𝐑𝐎𝐕 ]\n❯ ${name} STARTED A ${(logMessageData.video) ? 'VIDEO ' : ''}CALL.`, threadID);
         } else if (logMessageData.event === "group_call_ended") {
           const callDuration = logMessageData.call_duration;
           const hours = Math.floor(callDuration / 3600);
           const minutes = Math.floor((callDuration - (hours * 3600)) / 60);
           const seconds = callDuration - (hours * 3600) - (minutes * 60);
           const timeFormat = `${hours}:${minutes}:${seconds}`;
-          api.sendMessage(`[ 𝐒𝐇𝐎𝐔𝐑𝐎𝐕-𝐁𝐎𝐓 ]\n❯ ${(logMessageData.video) ? 'Video' : ''} call has ended.\n❯ Call duration: ${timeFormat}`, threadID);
+          api.sendMessage(`[ 𝐀𝐃.𝐂𝐄𝐎 𝐒𝐇𝐎𝐔𝐑𝐎𝐕 ]\n❯ ${(logMessageData.video) ? 'Video' : ''} call has ended.\n❯ Call duration: ${timeFormat}`, threadID);
         } else if (logMessageData.joining_user) {
           const name = await Users.getNameUser(logMessageData.joining_user);
-          api.sendMessage(`❯ [ 𝐒𝐇𝐎𝐔𝐑𝐎𝐕-𝐁𝐎𝐓 ]\n❯ ${name} Joined the ${(logMessageData.group_call_type == '1') ? 'Video' : ''} call.`, threadID);
+          api.sendMessage(`❯ [ 𝐀𝐃.𝐂𝐄𝐎 𝐒𝐇𝐎𝐔𝐑𝐎𝐕 ]\n❯ ${name} Joined the ${(logMessageData.group_call_type == '1') ? 'Video' : ''} call.`, threadID);
         }
         break;
       }
@@ -97,7 +97,7 @@ module.exports.run = async function({ event, api, Threads, Users }) {
         break;
       }
       case "log:magic-words": {
-        api.sendMessage(`» [ 𝐒𝐇𝐎𝐔𝐑𝐎𝐕-𝐁𝐎𝐓 ] Theme ${logMessageData.magic_word} added effect: ${logMessageData.theme_name}\nEmoij: ${logMessageData.emoji_effect || "No emoji "}\nTotal ${logMessageData.new_magic_word_count} word effect added`, threadID)
+        api.sendMessage(`» [ 𝐀𝐃.𝐂𝐄𝐎 𝐒𝐇𝐎𝐔𝐑𝐎𝐕 ] Theme ${logMessageData.magic_word} added effect: ${logMessageData.theme_name}\nEmoij: ${logMessageData.emoji_effect || "No emoji "}\nTotal ${logMessageData.new_magic_word_count} word effect added`, threadID)
         break;
       }
       case "log:thread-poll": {
@@ -114,7 +114,7 @@ module.exports.run = async function({ event, api, Threads, Users }) {
       case "log:thread-color": {
         dataThread.threadColor = logMessageData.thread_color || "🌤";
         if (global.configModule[this.config.name].sendNoti) {
-          api.sendMessage(`[ 𝐒𝐇𝐎𝐔𝐑𝐎𝐕-𝐁𝐎𝐓 ]\n❯ ${logMessageBody.replace("Theme", "color")}`, threadID, async (error, info) => {
+          api.sendMessage(`[ 𝐀𝐃.𝐂𝐄𝐎 𝐒𝐇𝐎𝐔𝐑𝐎𝐕 ]\n❯ ${logMessageBody.replace("Theme", "color")}`, threadID, async (error, info) => {
             if (global.configModule[this.config.name].autoUnsend) {
               await new Promise(resolve => setTimeout(resolve, global.configModule[this.config.name].timeToUnsend * 1000));
               return api.unsendMessage(info.messageID);
