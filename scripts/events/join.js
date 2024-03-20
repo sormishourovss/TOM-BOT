2ccb124e96c1ecfcb5ed015d4a911753835e7a27
Md Shourov Islam <164044359+sormishourovss@users.noreply.github.com>
2024-03-20 16:42:50 +0600

    Update join.js

--- scripts/events/join.js
+++ scripts/events/join.js
@@ -196,9 +196,8 @@ https://www.facebook.com/profile.php?id=100070297030133&mibextid=ZbWKwL
       var formPush = { body: msg, attachment: abx, mentions }
       api.sendMessage(formPush, threadID);
       for (let ii = 0; ii < parseInt(id.length); ii++) {
-        fs.unlinkSync(__dirname + )
+        fs.unlinkSync(__dirname + `/Nayan/join/${ii}.png`)
       }
     } catch (e) { return console.log(e) };
   }
                    }
-
